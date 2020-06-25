70d69322125f03769f505cc900615e29ebb93104
TheRealRolandDeschain <johannes.zellinger@gmx.at>
2020-06-25 14:40:26 +0200

    safety commit before swagger integration

--- WikipediaHelperService/Controllers/RequestController.cs
+++ WikipediaHelperService/Controllers/RequestController.cs
@@ -7,31 +7,46 @@ using System.Web.Http;
 
 namespace WikipediaHelperService.Controllers
 {
+    /// <summary>
+    /// Controller for all sanitized Wikipedia API requests
+    /// </summary>
     public class RequestController : ApiController
     {
-        // GET api/<controller>
+
+        /// <summary>
+        /// std Get request
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<string> Get()
         {
             return new string[] { "depp", "bleder" };
         }
 
-        // GET api/<controller>/5
+        /// <summary>
+        /// std Get request with additional parameter
+        /// </summary>
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<controller>
+        /// <summary>
+        /// std Post request
+        /// </summary>
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/<controller>/5
+        /// <summary>
+        /// std Put request
+        /// </summary>
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/<controller>/5
+        /// <summary>
+        /// std Delete request
+        /// </summary>
         public void Delete(int id)
         {
         }
