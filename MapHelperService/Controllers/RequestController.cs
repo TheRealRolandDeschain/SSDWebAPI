89f2490cb1a477edf353cea62727965d1724755d
TheRealRolandDeschain <johannes.zellinger@gmx.at>
2020-06-29 12:08:17 +0200

    wikimediahelper running now in docker started maphelperservice

--- MapHelperService/Controllers/RequestController.cs
+++ MapHelperService/Controllers/RequestController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WikiMediaHelperService.Helpers;
-using WikiMediaHelperService.Models;
 
-namespace WikiMediaHelperService.Controllers
+namespace MapHelperService.Controllers
 {
     /// <summary>
     /// Controller for all sanitized Wikipedia API requests
